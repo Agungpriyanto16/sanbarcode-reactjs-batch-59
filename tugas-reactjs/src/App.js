32f60682dec6907399961547211f93c2626fed59
Agungpriyanto16 <agungpriyanto160203@gmail.com>
2024-08-28 02:18:02 +0700

    Tugas hari ke-6

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+      <h1>Hi, Saya Agung Priyanto</h1>
+      <p>Saya dari Batch 59 sudah berhasil menginstall React</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
